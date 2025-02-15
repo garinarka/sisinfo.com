647d6cbea585be7f685774c363a4e7a30f1e4e64
garinarka <129123220+garinarka@users.noreply.github.com>
2025-02-15 07:39:52 +0700

    update routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,9 +12,25 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/profile', [Controllers\ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [Controllers\ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [Controllers\ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+Route::middleware(['auth'])->group(function () {
+    // Books
+    Route::resource('books', Controllers\BookController::class);
+
+    // Book Loans
+    Route::post('/loans', [Controllers\BookLoanController::class, 'store'])->name('loans.store');
+    Route::patch('/loans/{bookLoan}/return', [Controllers\BookLoanController::class, 'return'])
+        ->name('loans.return')
+        ->middleware('can:return,bookLoan');
+
+    // Notifications
+    Route::get('/notifications', function () {
+        return view('notifications.index');
+    })->name('notifications.index');
+});
+
+require __DIR__ . '/auth.php';
