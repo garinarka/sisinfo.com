08648eca751a612e0886ce2ccf7438bb1a304f28
garinarka <129123220+garinarka@users.noreply.github.com>
2025-02-15 11:21:23 +0700

    update bootstrap config

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,13 +1,15 @@
 <?php
 
+use App\Http\Controllers\BookLoanController;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Console\Scheduling\Schedule;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
@@ -15,4 +17,10 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
+    })
+    ->withSchedule(function (Schedule $schedule) {
+        $schedule->call(function () {
+            $controller = new BookLoanController();
+            $controller->checkDueBooks();
+        })->daily();
     })->create();
